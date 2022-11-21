9034fd94f0f33cec358eead23b5dcae2038d2f55
Rj <rj.salazar7350@gmail.com>
2022-11-21 13:31:31 -0600

    add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT sandbox(int32_value, bool_value, double_value) VALUES (234, 0, -123.123);
+
+--UPDATE sandbox SET int64_value = 12345 WHERE id = 25;
+
+--DELETE FROM sandbox WHERE id = 36;
